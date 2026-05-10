8ec0b4d9737d4f404bdd296040e2d1e25f152048
Thai Son Bui <buithaison2005@gmail.com>
2026-05-10 22:31:45 +0700

    testing

--- backend/src/controllers/reminder.controller.js
+++ backend/src/controllers/reminder.controller.js
@@ -53,9 +53,10 @@ const reminderController = {
     // DELETE /reminders/:id
     delete: async (req, res, next) => {
         try {
+            console.log(" CONTROLLER Đã vào hàm Delete với ID:", req.params.id);
             const userId = Number(req.user.user_id);
             const reminderId = Number(req.params.id);
-
+            
 
             await reminderService.deleteReminder(reminderId, userId);
 
