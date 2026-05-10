c9548218a9c68e3fbb2a94f285145d9b6ef52365
Thai Son Bui <buithaison2005@gmail.com>
2026-05-10 23:02:40 +0700

    fix mail config

--- backend/src/config/mail.config.js
+++ backend/src/config/mail.config.js
@@ -8,5 +8,9 @@ export const transporter = nodemailer.createTransport({
     auth: {
         user: process.env.SMTP_USER,
         pass: process.env.SMTP_PASS
-    }
+    },
+    family: 4, //
+    connectionTimeout: 10000, // 
+    greetingTimeout: 5000,
+    socketTimeout: 15000,
 });
